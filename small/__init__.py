91ed019e21d0110c1dfcad5196c1a6f6457b33eb
Nick Srivastava <nicksriv@outlook.com>
2026-01-27 09:09:56 -0800

    Add training data helpers and vocab planning

--- small/__init__.py
+++ small/__init__.py
@@ -4,6 +4,8 @@ from .compressor import compress, compress_python_source, decompress
 from .config import CompressionConfig
 from .engine import CompressionEngine, default_engine
 from .sequence import TokenSequence
+from .training import TrainingExample, build_example, build_curriculum, generate_training_examples
+from .vocab import VocabExtension, plan_vocab_extension
 from .types import CompressionResult
 
 __all__ = [
@@ -15,4 +17,10 @@ __all__ = [
     "CompressionEngine",
     "TokenSequence",
     "default_engine",
+    "TrainingExample",
+    "build_example",
+    "build_curriculum",
+    "generate_training_examples",
+    "VocabExtension",
+    "plan_vocab_extension",
 ]
